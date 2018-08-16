dd6bf32996bbd465f120c0b2f030fa3f9ef13aba
luan0ap <luanap.kun@gmail.com>
2018-08-16 13:14:48 -0300

    created object to colors

--- data/js/trafficlight.js
+++ data/js/trafficlight.js
@@ -1,25 +1,33 @@
 const $canvas = document.querySelector('#canvas')
 const ctx = $canvas.msGetInputContext('2d')
 
+const sequence = ['red', 'yellow', 'green']
+
+const colors = {
+	red: () => {
+		ctx.fillStyle = '#ff0000'
+		ctx.beginPath()
+		ctx.arc(400, 110, 90, 0, 2 * Math.PI, false)
+		ctx.fill()
+	},
+	yellow: () => {
+		ctx.fillStyle = '#ffff00'
+		ctx.beginPath()
+		ctx.arc(400, 300, 90, 0, 2 * Math.PI, false)
+		ctx.fill()
+	},
+	green: () => {
+		ctx.fillStyle = '#00ff00'
+		ctx.beginPath()
+		ctx.arc(400, 490, 90, 0, 2 * Math.PI, false)
+		ctx.fill()
+	}
+}
+
 function draw() {
 	ctx.fillStyle = '#666'
 	ctx.fillRect(0, 0, 800, 600)
-
-	ctx.fillStyle = '#ff0000'
-	ctx.beginPath()
-	ctx.arc(400, 110, 90, 0, 2 * Math.PI, false)
-	ctx.fill()
-
-	ctx.fillStyle = '#ffff00'
-	ctx.beginPath()
-	ctx.arc(400, 300, 90, 0, 2 * Math.PI, false)
-	ctx.fill()
-
-	ctx.fillStyle = '#00ff00'
-	ctx.beginPath()
-	ctx.arc(400, 490, 90, 0, 2 * Math.PI, false)
-	ctx.fill()
-
+	
 }
 
 // function resize() {
