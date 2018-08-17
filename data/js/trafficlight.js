e856061979681651b9d4be2dec200ab881701242
luan0ap <luanap.kun@gmail.com>
2018-08-17 14:55:29 -0300

    created animation

--- data/js/trafficlight.js
+++ data/js/trafficlight.js
@@ -1,34 +1,41 @@
+const delayForEach = (arr, delay, cb) => arr.forEach((x, i) => setTimeout(cb.bind(cb, x), delay * i))
+
 const $canvas = document.querySelector('#canvas')
-const ctx = $canvas.msGetInputContext('2d')
+const ctx = $canvas.getContext('2d')
 
 const sequence = ['red', 'yellow', 'green']
 
-const colors = {
-	red: () => {
-		ctx.fillStyle = '#ff0000'
-		ctx.beginPath()
-		ctx.arc(400, 110, 90, 0, 2 * Math.PI, false)
-		ctx.fill()
-	},
-	yellow: () => {
-		ctx.fillStyle = '#ffff00'
-		ctx.beginPath()
-		ctx.arc(400, 300, 90, 0, 2 * Math.PI, false)
-		ctx.fill()
-	},
-	green: () => {
-		ctx.fillStyle = '#00ff00'
-		ctx.beginPath()
-		ctx.arc(400, 490, 90, 0, 2 * Math.PI, false)
-		ctx.fill()
-	}
-}
 
-function draw() {
+const draw = (color) => {
+	
 	ctx.fillStyle = '#666'
 	ctx.fillRect(0, 0, 800, 600)
-	
-}
+
+	const colors = {
+		red: () => {
+			ctx.fillStyle = '#ff0000'
+			ctx.beginPath()
+			ctx.arc(400, 110, 90, 0, 2 * Math.PI, false)
+			ctx.fill()
+		},
+		yellow: () => {
+			ctx.fillStyle = '#ffff00'
+			ctx.beginPath()
+			ctx.arc(400, 300, 90, 0, 2 * Math.PI, false)
+			ctx.fill()
+		},
+		green: () => {
+			ctx.fillStyle = '#00ff00'
+			ctx.beginPath()
+			ctx.arc(400, 490, 90, 0, 2 * Math.PI, false)
+			ctx.fill()
+		}
+	}
+
+	return colors[color]() 
+} 
+
+delayForEach(sequence, 1000, draw)
 
 // function resize() {
 // 	var height = window.innerHeight
